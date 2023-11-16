5c797369d2b1c1cffb1323a6d1bfa432e4071d9c
Lucas Konstantin Bärenfänger <18075314+typeduke@users.noreply.github.com>
2023-11-16 15:20:07 +0000

    Add userinfo.profile scope

--- uber_pickups.py
+++ uber_pickups.py
@@ -16,7 +16,7 @@ REVOKE_TOKEN_URL = "https://accounts.google.com/o/oauth2/auth"
 CLIENT_ID = "726921588578-gqa9vn25qk4sm8pr1uis5be3v3m9puu5.apps.googleusercontent.com"
 CLIENT_SECRET = "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"
 REDIRECT_URI = "https://streamlit-template.k8s.aws.tadnet.net"
-SCOPE = "openid https://www.googleapis.com/auth/userinfo.email"
+SCOPE = "openid https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile"
 
 # Create OAuth2Component instance
 oauth2 = OAuth2Component(CLIENT_ID, CLIENT_SECRET, AUTHORIZE_URL, TOKEN_URL, REFRESH_TOKEN_URL, REVOKE_TOKEN_URL)
